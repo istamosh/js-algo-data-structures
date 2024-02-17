77a9695ac8b9e1fe8fe494f3860eb15ea0cd5fc6
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-17 14:54:12 +0700

    Calorie Counter Step 25

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -8,10 +8,17 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-    const strArray = str.split('');
-    const cleanStrArray = [];
+    // const strArray = str.split('');
+    // const cleanStrArray = [];
 
-    for (let i = 0; i < strArray.length; i++) {
+    // for (let i = 0; i < strArray.length; i++) {
+    //     if (!['+', '-', ' '].includes(strArray[i])) {
+    //         cleanStrArray.push(strArray[i]);
+    //     }
+    // }
 
-    }
+    // using regular expression to reduce memory usage
+    // looking a + and - sign using \ as the escaping symbol to recognize them
+    // \s is a white space like " "
+    const regex = /\+-\s/;
 }
