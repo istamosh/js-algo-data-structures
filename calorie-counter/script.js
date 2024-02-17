02529f39fe526b97132412768ba1711a6c03bc9d
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-17 18:51:27 +0700

    Step 32

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -20,5 +20,19 @@ function cleanInputString(str) {
     // using regular expression to reduce memory usage
     // looking a + and - sign using \ as the escaping symbol to recognize them
     // \s is a white space like " "
-    const regex = /\+-\s/;
+    // const regex = /\+-\s/;
+
+    // using regex but with character class, wrapped in square brackets
+    // after wrapping with /" "/, you can add "g" as global flag.
+    const regex = /[+-\s]/g;
+
+    // how to replace the characters from params. with above regex
+    return str.replace(regex, "");
+}
+
+function isInvalidInput(str) {
+    // if the regex have only 1 character to filter, then it no longer requires []
+    // i flag stands for insensitive, meaning no matter the upper/lower case, it means the same
+    // the regex will catch the e in-between any numeral inputs (e.g. 1e1, 2e9, 7e4)
+    const regex = /[0-9]e[0-9]/i;
 }
