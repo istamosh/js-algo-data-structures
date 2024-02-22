8cd65275d030f4d146a5ff9e15e5dbb86e6f97bc
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-22 21:00:41 +0700

    Palindrome checker: backslash reading is still bad

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -2,62 +2,53 @@ const textInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-// chops every character into an array
-const chops = char => char.split("");
-
-// use two indexes interation (i and j)
+// use two indexes iteration
 const test = char => {
-    console.clear();
-    console.log(`Char length: ${char.length}`);
-    console.log(`Char length /2 roundup: ${Math.ceil(char.length /2)}`);
-    console.log(`Array: ${char.split("")}`)
-
-    // var halfCoverage = Math.ceil(char.length /2);
-
-    // var halfString = "";
-    // var revHalfString = "";
-    // // now get the half strings
-    // for (var i = 0; i < halfCoverage; i++) {
-    //     halfString += char[i];
-    // }
-    // // and get the reversed one
-    // if (char.length % 2 == 0) {
-    //     for (var i = char.length-1; i > halfCoverage -1; i--) {
-    //         revHalfString += char[i];
-    //     }
-    // } else {
-    //     for (var i = char.length-1; i > halfCoverage -2; i--) {
-    //         revHalfString += char[i];
-    //     }
-    // }
-    // console.log(halfString);
-    // console.log(revHalfString);
-
-    // // iterate half of input
-    // // provide more lenient halfs
-    // var lenientHalfString = "";
-    // var lenientRevHalfString = "";
-    // for (var i = 1; i <= halfCoverage; i++) {
-    //     // then replace string for check so it's more lenient
-
-    //     if (halfString[i] == revHalfString[i]) {
-
-    //     }
-    // }
-
+    // remove symbols and whitespaces using regex
+    const regex = / |_|-|,|\?|\.|\!/g;
+    // add regex for opposite symbol
+    const part        = /\(|\[|\\|<|{/g;
+    const counterpart = /\)|\]|\/|>|}/g;
+    let processed = char.replace(regex, "").toLowerCase();
     // define the two indexes
     let forward = 0;
-    let backward = char.length -1;
+    let backward = processed.length -1;
+
+    let word1 = "";
+    let word2 = "";
     // and run the iteration simultaneously until meet midway
     while (forward < backward) {
-        
+        word1 += processed[forward];
+        word2 += processed[backward];
+        // compare forward and backward iteration one at a time, if it's not the same then the iteration will stop
+        innerloop: if (processed[forward] != processed[backward]) {
+            if (part.test(processed[forward]) && counterpart.test(processed[backward])) {
+                console.log('1');
+                break innerloop;
+            }
+            else if (counterpart.test(processed[forward]) && part.test(processed[backward])) {
+                console.log('2');
+                break innerloop;
+            }
+            console.log(word1);
+            console.log(word2);
+            console.log(part.test(processed[forward]) && counterpart.test(processed[backward]));
+            console.log(counterpart.test(processed[forward]) && part.test(processed[backward]));
+
+            return false;
+        }
+        forward++;
+        backward--;
     }
+    console.log(word1);
+    console.log(word2);
+    return true;
 };
 
 // takes input, and detects if box is empty or using whitespace only
 checkButton.addEventListener('click', () => {
     // result.innerHTML = test(textInput.value)
-    test(textInput.value)
+    result.innerText = test(textInput.value);
 });
 
 
