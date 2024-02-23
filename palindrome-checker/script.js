0d4d2ddd1015793e4b85912f92d759925a8cee7c
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-23 18:31:28 +0700

    Palindrome Checker: Finalizing styles

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -1,6 +1,7 @@
 const textInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
+const clearButton = document.getElementById('clear');
 
 // use two indexes iteration
 const checkPalindrome = input => {
@@ -13,24 +14,40 @@ const checkPalindrome = input => {
     // and run the iteration simultaneously until meet midway
     while (forward < backward) {
         // compare forward and backward iteration one at a time, if it's not the same then the iteration will stop
-        if (processed[forward] != processed[backward]) {
-            return false;
-        }
+        if (processed[forward] != processed[backward]) return false;
         forward++;
         backward--;
     }
     return true;
 };
 
-// takes input, and detects if box is empty or using whitespace only
-checkButton.addEventListener('click', () => {
+// execute input
+const execute = () => {
     // check for empty spaces without any characters and symbols on input
     const regex = /^\s+$/g;
     if (textInput.value == "" || textInput.value.match(regex)) {
         alert("Please input a value");
+        textInput.value = "";
         return false;
     }
 
     if (checkPalindrome(textInput.value)) result.insertAdjacentHTML('afterbegin', `<span class="true">${textInput.value} is a palindrome</span>`);
     else result.insertAdjacentHTML('afterbegin', `<span class="false">${textInput.value} is not a palindrome</span>`);
-});
+    textInput.value = "";
+};
+
+// clear history
+const clear = () => {
+    let container = document.getElementById('result');
+    let spans = container.getElementsByTagName('span'), i;
+
+    if (spans.length === 0) return false;
+    else for (i = spans.length - 1; i >= 0; i--) 
+            spans[i].parentNode.removeChild(spans[i]);
+};
+
+// takes input, and detects if box is empty or using whitespace only
+checkButton.addEventListener('click', execute);
+
+// clear history
+clearButton.addEventListener('click', clear);
