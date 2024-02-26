55e13ddd51e0912dc90495b74de2bc05489c2914
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-26 15:55:10 +0700

    Football cards: Step 5

--- football-team-cards/script.js
+++ football-team-cards/script.js
@@ -1,3 +1,9 @@
 const teamName = document.getElementById('team');
 const typeOfSport = document.getElementById('sport');
 const worldCupYear = document.getElementById('year');
+const headCoach = document.getElementById('head-coach');
+const playerCards = document.getElementById('player-cards');
+const playersDropdownList = document.getElementById('players');
+
+// build out the data structure that will hold all of the information for your football team
+const myFavoriteFootballTeam = {};
