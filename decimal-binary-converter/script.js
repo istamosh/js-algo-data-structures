21823f97d0cbf943b6ca04323283a81f8be018d6
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-29 10:28:24 +0700

    Decimal-Binary converter: done

--- decimal-binary-converter/script.js
+++ decimal-binary-converter/script.js
@@ -23,6 +23,34 @@
 const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
+const animationContainer = document.getElementById('animation-container');
+
+const animationData = [
+    {
+        inputVal: 5,
+        marginTop: 300,
+        addElDelay: 1000,
+        msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+        showMsgDelay: 15000,
+        removeElDelay: 20000
+    },
+    {
+        inputVal: 2,
+        marginTop: -200,
+        addElDelay: 1500,
+        msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+        showMsgDelay: 10000,
+        removeElDelay: 15000
+    },
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000,
+        msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
+        showMsgDelay: 5000,
+        removeElDelay: 10000
+    }
+];
 
 const decimalToBinary = input => {
     // // 1st method
@@ -61,26 +89,70 @@ const decimalToBinary = input => {
     // }
     // result.innerText = binary;
 
-    if (input === 0) {
-        return '0';
+    if (input === 0 || input === 1) {
+        return String(input);
     } else {
         return decimalToBinary(Math.floor(input /2)) + (input %2);
     }
 };
 
+const showAnimation = () => {
+    // // add half sec delay
+    // setTimeout(() => { console.log('free'); }, 500);
+
+    // // give 1 sec delay before execute console "Code" (async.)
+    // setTimeout(() => { console.log('Code'); }, 1000);
+    
+    // setTimeout(() => { console.log('Camp'); }, 1500);
+
+    result.innerText = 'Call Stack Animation';
+
+    animationData.forEach(obj => {
+        setTimeout(() => {
+            animationContainer.innerHTML += `
+            <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+            decimalToBinary(${obj.inputVal})
+            </p>
+            `;
+        }, obj.addElDelay);
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove();
+        }, obj.removeElDelay);
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).textContent = obj.msg;
+            obj.msg.textContent
+        }, obj.showMsgDelay);
+    });
+
+    setTimeout(() => {
+        result.textContent = decimalToBinary(5);
+    }, 20000);
+};
+
 // do some setup to check the value in the number input element whenever the user clicks the Convert button.
 const checkUserInput = () => {
+    // parse input firsthand to prevent falsy integer input
+    const inputInt = parseInt(numberInput.value);
+
     // handle if input box is empty
     // you should handle falsy input like null, 0, and undefined, instead of using === '', use ! as the prefix
     // use parseInt() to prevent number like 2.3, 2e+3, or NaN (because of "e" only)
     // check if the input is not a number, isNaN()
-    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    if (!numberInput.value || isNaN(inputInt)) {
         alert('Please provide a decimal number');
         return;
     }
 
+    // check if input is strictly equal to 5
+    if (inputInt === 5) {
+        showAnimation();
+        return;
+    }
+
     // pass parsed value to another function and show to result
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = '';
 };
 
