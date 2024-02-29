5f0057be230089f742d2c5e282cd8e934dd7924a
istamosh <63596969+istamosh@users.noreply.github.com>
2024-02-29 21:36:40 +0700

    Roman Numeral Converter: basic input and button workarounds

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -1,3 +1,31 @@
 const inputBox = document.getElementById('number');
 const convertButton = document.getElementById('convert-btn');
 const outputDiv = document.getElementById('output');
+
+// using roman table to determine string output
+// using remainder/modulo operator %
+
+const check = () => {
+    if (inputBox.value === '') {
+        alert('Please enter a valid number')
+        return;
+    }
+
+    const input = inputBox.value;
+
+    const span = document.createElement('span')
+    const textNode = document.createTextNode('hehe')
+
+    span.appendChild(textNode)
+
+    if (outputDiv.hasChildNodes()) {
+        outputDiv.innerHTML = '';
+    }
+    outputDiv.appendChild(span)
+}
+
+convertButton.addEventListener('click', check);
+inputBox.addEventListener('keydown', e => {
+    if (e.key === 'Enter') check();
+    else return;
+})
