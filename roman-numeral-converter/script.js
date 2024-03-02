c43ee7b9953171c79d5bdf6287aa92bacd356456
istamosh <63596969+istamosh@users.noreply.github.com>
2024-03-02 17:58:45 +0700

    Roman Numeral Converter: done building the algorithm

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -2,20 +2,46 @@ const inputBox = document.getElementById('number');
 const convertButton = document.getElementById('convert-btn');
 const outputDiv = document.getElementById('output');
 
-// using roman table to determine string output
-// using remainder/modulo operator %
-
 const check = () => {
     if (inputBox.value === '') {
         alert('Please enter a valid number')
         return;
     }
 
-    const input = inputBox.value;
+    let input = inputBox.value;
 
+    // define the roman numeral list
+    const romanList = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    // provide an output
     let testText = '';
-    for (let i = 0; i < input.length; i++) {
-        testText += `,${input[i]}`;
+
+    // check for all possibilities inside a list
+    for (let i in romanList) {
+        // compare input if it's the same or higher with every possible object
+        while (input >= romanList[i]) {
+            // write an output roman letter
+            testText += i;
+
+            // then subtract the input value w the value of compared object
+            input -= romanList[i];
+
+            // rinse and repeat back to while loop until it hits zero
+        }
     }
 
     const span = document.createElement('span')
