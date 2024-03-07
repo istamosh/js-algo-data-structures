df1c959b93f9d97291de1dae7e6892e06e6f9aab
istamosh <63596969+istamosh@users.noreply.github.com>
2024-03-08 00:22:52 +0700

    Number Sorter: step 10

--- number-sorter/script.js
+++ number-sorter/script.js
@@ -4,7 +4,16 @@ const sortInputArray = event => {
     // buttons associated with a form element submit by default, you need to prevent that behavior
     event.preventDefault();
 
-    const inputValues = document.getElementsByClassName('values-dropdown');
+    // spread the array result directly
+    // map the array w/ dropdown params and return its value
+    const inputValues = [...document.getElementsByClassName('values-dropdown')].map(dropdown => Number(dropdown.value));
+};
+
+// declaring funct. that have own param with fallback value of an array
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+    });
 };
 
 sortButton.addEventListener('click', sortInputArray);
