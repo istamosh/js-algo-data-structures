d916510827391fbbab6028fb42e8eaba1b59583f
istamosh <63596969+istamosh@users.noreply.github.com>
2024-03-09 22:03:46 +0700

    Statistic calculator: step 12

--- statistic-calculator/script.js
+++ statistic-calculator/script.js
@@ -1,3 +1,9 @@
+const getMean = array => {
+    // takes accumulator and element parameter
+    // set accumulator to zero
+    const sum = array.reduce((acc, el) => acc + el, 0);
+}
+
 const calculate = () => {
     const value = document.querySelector('#numbers').value;
     
@@ -7,7 +13,7 @@ const calculate = () => {
     // map creates a new array instead of mutating the original array
     // The callback function needs to return a value. In this case, you want to return the value of each element converted to a number. You can do this by using the Number() constructor, passing the element as an argument.
     // Add a callback function to your .map() method that converts each element to a number.
-    const numbers = array.map(el => Number(el));
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 }
 
 // calculate();
