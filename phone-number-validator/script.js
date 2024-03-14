a3ea5ff740d2225e92a3121ebb176bc52c5df6aa
istamosh <63596969+istamosh@users.noreply.github.com>
2024-03-14 19:26:41 +0700

    Number Validator: beginning the project

---
+++ phone-number-validator/script.js
@@ -0,0 +1,5 @@
+const inputBox = document.getElementById('user-input')
+const checkButton = document.getElementById('check-btn')
+const clearButton = document.getElementById('clear-btn')
+const resultDiv = document.getElementById('results-div')
+
