480da1374c463bcb5fbe0611090ce9d2706efb01
istamosh <63596969+istamosh@users.noreply.github.com>
2024-03-23 19:30:03 +0700

    Phone number validator: completed

--- phone-number-validator/script.js
+++ phone-number-validator/script.js
@@ -3,39 +3,12 @@ const checkButton = document.getElementById('check-btn')
 const clearButton = document.getElementById('clear-btn')
 const resultDiv = document.getElementById('results-div')
 
-const numpad1 = document.getElementById('numpad1');
-const numpad2 = document.getElementById('numpad2');
-const numpad3 = document.getElementById('numpad3');
-const numpad4 = document.getElementById('numpad4');
-const numpad5 = document.getElementById('numpad5');
-const numpad6 = document.getElementById('numpad6');
-const numpad7 = document.getElementById('numpad7');
-const numpad8 = document.getElementById('numpad8');
-const numpad9 = document.getElementById('numpad9');
-const numpad0 = document.getElementById('numpad0');
-const numpadOpenBr = document.getElementById('numpad(');
-const numpadCloseBr = document.getElementById('numpad)');
-const numpadMinus = document.getElementById('numpad-');
-
-const keyTable = {
-    '0': 48,
-    '1': 49,
-    '2': 50,
-    '3': 51,
-    '4': 52,
-    '5': 53,
-    '6': 54,
-    '7': 55,
-    '8': 56,
-    '9': 57,
-    '-': [109, 189],
-    '(': 219,
-    ')': 221
-}
-
-numpad0.addEventListener('click', () => {
-    inputBox.value += '0';
-    console.log('pressed 0');
+// me being lazy to add multiple click listeners based on every buttons' textContent
+const numpad = document.querySelectorAll('[id*="numpad"]');
+numpad.forEach(elements => {
+    elements.addEventListener('click', () => {
+        inputBox.value += elements.textContent;
+    })
 })
 
 const isEmpty = () => {
@@ -67,9 +40,15 @@ const check = () => {
     // prepare the child div
     const p = document.createElement('p');
     p.className = 'result-text';
-    p.id = is[0];
+    
     p.appendChild(document.createTextNode(`${is[1]} US number: ${trimmedInput}`))
-    resultDiv.appendChild(p);
+    // prepare the wrapper div
+    const div = document.createElement('div');
+    div.className = 'border-wrap';
+    div.id = is[0];
+    div.appendChild(p);
+    // insert new child as the first element
+    resultDiv.insertBefore(div, resultDiv.children[0])
 
     inputBox.value = '';
 }
