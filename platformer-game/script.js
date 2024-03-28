841e58acb14e635cbcef1c8a9cb990c4f086e8ff
istamosh <63596969+istamosh@users.noreply.github.com>
2024-03-28 18:14:01 +0700

    Platformer game: step 87

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -76,9 +76,80 @@ class Player {
     }
 }
 
+class Platform {
+    constructor(x, y) {
+        // using shorthand object x, y for the this.position
+        this.position = {
+            x, 
+            y
+        };
+        this.width = 200;
+        this.height = proportionalSize(40);
+    }
+    draw() {
+        ctx.fillStyle = '#acd157';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
 const player = new Player();
 
+const platformPositions = [
+    {
+        x: 500,
+        y: proportionalSize(450)
+    },
+    {
+        x: 700,
+        y: proportionalSize(400)
+    },
+
+    {
+        x: 850,
+        y: proportionalSize(350)
+    },
+    {
+        x: 900,
+        y: proportionalSize(350)
+    },
+    {
+        x: 1050,
+        y: proportionalSize(150)
+    },
+    {
+        x: 2500,
+        y: proportionalSize(450)
+    },
+    {
+        x: 2900,
+        y: proportionalSize(400)
+    },
+    {
+        x: 3150,
+        y: proportionalSize(350)
+    },
+    {
+        x: 3900,
+        y: proportionalSize(450)
+    },
+    {
+        x: 4200,
+        y: proportionalSize(400)
+    },
+    {
+        x: 4400,
+        y: proportionalSize(200)
+    },
+    {
+        x: 4700,
+        y: proportionalSize(150)
+    }
+];
+
+const platforms = platformPositions.map(platform => new Platform(platform.x, platform.y));
+
 const animate = () => {
+    //#region 
     // The requestAnimationFrame() web API, takes in a callback and is used to update the animation on the screen.
     // The animate function will be responsible for updating the player's position and continually drawing it on the canvas.
     requestAnimationFrame(animate);
@@ -86,7 +157,47 @@ const animate = () => {
     // You can use the clearRect() Web API to accomplish this. It takes in an x, y, width, and height arguments.
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // call draw method on every platform elements inside the platforms array
+    platforms.forEach(platform => {
+        platform.draw();
+    });
+
     player.update();
+    //#endregion
+    if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+        player.velocity.x = 5;
+    } else if (keys.leftKey.pressed && player.position.x > proportionalSize(100)) {
+        player.velocity.x = -5;
+    } else {
+        player.velocity.x = 0;
+
+        if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5;
+            });
+        } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+            platforms.forEach(platform => {
+                platform.position.x += 5;
+            });
+        }
+    }
+
+    platforms.forEach(platform => {
+        const collisionDetectionRules = [
+            player.position.y + player.height <= platform.position.y,
+            player.position.y + player.height + player.velocity.y >= platform.position.y,
+            player.position.x >= platform.position.x - player.width / 2,
+            player.position.x <= platform.position.x + platform.width - player.width / 3
+        ];
+
+        // check if every element of collision rules array is true
+        if (collisionDetectionRules.every(rule => rule)) {
+            player.velocity.y = 0;
+            return;
+        }
+
+        const platformDetectionRules = []
+    });
 }
 
 const keys = {
@@ -94,11 +205,48 @@ const keys = {
     leftKey: {pressed: false},
 }
 
+const movePlayer = (key, xVelocity, isPressed) => {
+    if (!isCheckpointCollisionDetectionActive) {
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        return;
+    }
+
+    switch (key) {
+        case "ArrowLeft":
+            keys.leftKey.pressed = isPressed;
+            if (xVelocity === 0) {
+                player.velocity.x = xVelocity;
+            }
+            player.velocity.x -= xVelocity;
+            break;
+        case "ArrowUp":
+        case " ":
+        case "Spacebar":
+            player.velocity.y -= 8;
+            break;
+        case "ArrowRight":
+            keys.rightKey.pressed = isPressed;
+            if (xVelocity === 0) {
+                player.velocity.x = xVelocity;
+            }
+            player.velocity.x += xVelocity;
+    }
+}
+
 const startGame = () => {
     canvas.style.display = 'block';
     startScreen.style.display = 'none';
 
-    player.draw();
+    animate();
 }
 
 startBtn.addEventListener('click', startGame);
+
+window.addEventListener('keydown', ({ key }) => {
+    movePlayer(key, 8, true)
+})
+
+window.addEventListener('keyup', ({ key }) => {
+    movePlayer(key, 0, false)
+})
