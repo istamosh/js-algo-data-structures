a8ec573b150a33cf1e5c1322a9604d91f7397fa7
istamosh <63596969+istamosh@users.noreply.github.com>
2024-04-01 16:30:42 +0700

    Cash register: beginning project

---
+++ cash-register/script.js
@@ -0,0 +1,25 @@
+//#region The mandatory codeblock, do not change
+
+let price = 1.87;
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+];
+
+//#endregion
+
+const cashNumber = document.getElementById('cash');
+const purchaseButton = document.getElementById('purchase-btn');
+
+const purchase = () => {
+    const cash = cashNumber.textContent;
+}
+
+purchaseButton.addEventListener('click', () => {})
