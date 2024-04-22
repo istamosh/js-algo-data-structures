2ea4c119e4eabd6a3b9d8dff29f397eaf58f995d
istamosh <63596969+istamosh@users.noreply.github.com>
2024-04-22 21:25:14 +0700

    Pokemon app: testing out API

--- pokemon-search-app/script.js
+++ pokemon-search-app/script.js
@@ -1,1 +1,16 @@
-// Script comes here
+const endpoint = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(endpoint)
+        console.log(res)
+        const data = await res.json()
+        const {_ , results} = data;
+        console.log(results)
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+fetchData()
