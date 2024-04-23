2c4182e411f1fc101f560c6f04c803de1fa7867b
istamosh <63596969+istamosh@users.noreply.github.com>
2024-04-23 22:00:21 +0700

    Pokemon app: added an API indicator

--- pokemon-search-app/script.js
+++ pokemon-search-app/script.js
@@ -1,16 +1,28 @@
-const endpoint = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
+const api = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
+const statusBar = document.getElementById('status-bar');
 
 const fetchData = async () => {
+    fetched = false;
     try {
-        const res = await fetch(endpoint)
-        console.log(res)
-        const data = await res.json()
+        const resource = await fetch(api)
+        console.log(resource)
+        const data = await resource.json()
         const {_ , results} = data;
         console.log(results)
+
+        fetched = true;
     }
     catch (e) {
         console.log(e)
+
+        fetched = false;
     }
+
+    setTimeout(() => {
+        statusBar.style.backgroundColor = fetched 
+        ? 'limegreen' 
+        : 'firebrick';
+    }, 1000)
 }
 
 fetchData()
