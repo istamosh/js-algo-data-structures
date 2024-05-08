c52dc3e7b63f261a2c57bb9a25d11e0e6aa0f8f8
istamosh <63596969+istamosh@users.noreply.github.com>
2024-05-08 19:05:25 +0700

    Database exercise: delete complete

--- database-exercise/script.js
+++ database-exercise/script.js
@@ -2,14 +2,25 @@ const table = document.querySelector('table');
 
 let storageArray = [];
 
+const prepareHeader = () => {
+    table.innerHTML = `
+    <tr>
+        <th>#</th>
+        <th>Nama Kegiatan</th>
+        <th>Edit Kegiatan</th>
+        <th>Hapus Kegiatan</th>
+    </tr>`;
+}
+
 const tabulateData = (array) => {
+    prepareHeader();
     array.forEach(element => {
         table.innerHTML += `
         <tr>
             <th>${element.id}</th>
             <td>${element.desc}</td>
             <td><button id="update-btn">Edit</button></td>
-            <td><button id="delete-btn">Hapus</button></td>
+            <td><button id="delete-btn" onclick="deleteEntry(${element.id})">Hapus</button></td>
         </tr>`
     });
 }
@@ -18,14 +29,15 @@ const create = () => {
     // detect vacant id
     let id = 0
     if (storageArray) {
-        for (let i = 0; i < storageArray.length; i++) {
-            if (i === storageArray[i].id) {
-                id++;
+        // toSorted() sort without mutating original array
+        // iterate every id, if id is the same, shift 1 to next iteration
+        storageArray.toSorted((a, b) => a.id - b.id)
+        .forEach(el => {
+            if (el.id !== id) {
+                return;
             }
-            else {
-                id = i;
-            }
-        }
+            id++;
+        })
     }
         
     // desc preset
@@ -41,21 +53,41 @@ const create = () => {
     localStorage.setItem('localDb', JSON.stringify(storageArray))
     console.log(`Successfully stored! Data => ${JSON.stringify(storageArray)}`)
 
-    // display only newly created data
-    tabulateData(new Array(newEntry));
+    tabulateData(storageArray);
 }
 
 const read = () => {
-    const hehe = JSON.parse(localStorage.getItem('localDb'));
-    console.log(hehe);
-    // localStorage.clear();
-    if (hehe) {
-        storageArray = hehe;
+    const payload = JSON.parse(localStorage.getItem('localDb'));
+
+    if (payload) {
+        // load data to local array for display
+        storageArray = payload;
         tabulateData(storageArray);
+        console.log(`Successfully read! Data => ${JSON.stringify(storageArray)}`)
     }
 }
 // trigger read on page loading
-document.addEventListener('DOMContentLoaded', read());
+document.addEventListener('DOMContentLoaded', () => {
+    prepareHeader();
+    read();
+});
+
+const deleteEntry = index => {
+    // point and delete database entry by index
+    // map: [id:1, id:2, id:3, ...]
+    // indexOf(3) = id:3 which is index no.2
+    // therefore pointer is pointing at 2
+    const pointer = storageArray.map(el => el.id).indexOf(index)
+    if (index !== -1) {
+        // remove entry started from index no. and amount of deletion starting from there
+        storageArray.splice(pointer, 1);
+        localStorage.setItem('localDb', JSON.stringify(storageArray))
+        console.log(`Successfully delete! Data => ${JSON.stringify(storageArray)}`)
+    }
+
+    // update the display
+    tabulateData(storageArray);
+}
 
 const testing = () => {
     let objArr = [
@@ -77,3 +109,8 @@ document.getElementById('create-btn').addEventListener('click', e => {
     e.preventDefault()
     create()
 })
+
+const clearLocalStorage = () => {
+    localStorage.clear();
+    console.log('localStorage cleared!, refresh page to take effect');
+}
