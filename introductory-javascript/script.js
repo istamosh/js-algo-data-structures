98689ef72f8e6539f246d936540163ffbb2ca726
istamosh <63596969+istamosh@users.noreply.github.com>
2024-05-11 23:09:38 +0700

    Intro JS: 20% mark

--- introductory-javascript/script.js
+++ introductory-javascript/script.js
@@ -1,1 +1,9 @@
-let character;
+let character = "Hello";
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+// dynamically access last element
+// rows[rows.length -1] = 10;
+
+rows.push("freeCodeCamp");
+let popped = rows.pop()
+console.log(popped);
