d15ef571f40bf0023a6959b8236d28d24cce6e70
istamosh <63596969+istamosh@users.noreply.github.com>
2024-05-12 21:08:36 +0700

    Intro JS: Step 65

--- introductory-javascript/script.js
+++ introductory-javascript/script.js
@@ -4,8 +4,16 @@ const rows = [];
 // dynamically access last element
 // rows[rows.length -1] = 10;
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1));
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
+
+for (let i = 0; i < count; i++) {
+  rows.push(padRow(i + 1, count));
 }
 
 let result = "";
