900e8bc409131d3080f3fbc97008d6610ec67908
istamosh <63596969+istamosh@users.noreply.github.com>
2024-05-17 16:10:34 +0700

    dom manipulation: complete

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,10 +4,7 @@ function getRandomComputerResult() {
   //   get array random index number based on array max length
   return options[Math.floor(Math.random() * options.length)];
 }
-console.log(getRandomComputerResult());
-
-let playerScore = 0;
-let computerScore = 0;
+// console.log(getRandomComputerResult());
 
 function hasPlayerWonTheRound(player, computer) {
   return (
@@ -16,8 +13,11 @@ function hasPlayerWonTheRound(player, computer) {
     (player === "Paper" && computer === "Rock")
   );
 }
-console.log(hasPlayerWonTheRound("Rock", "Scissors"));
-console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+// console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+// console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getRoundResults(userOption) {
   const computerResult = getRandomComputerResult();
@@ -25,7 +25,7 @@ function getRoundResults(userOption) {
   if (hasPlayerWonTheRound(userOption, computerResult)) {
     playerScore++;
     return `Player wins! ${userOption} beats ${computerResult}`;
-  } else if (userOption === computer) {
+  } else if (userOption === computerResult) {
     return `It's a tie! Both chose ${userOption}`;
   } else {
     computerScore++;
@@ -33,5 +33,61 @@ function getRoundResults(userOption) {
   }
 }
 
-console.log(getRoundResults("Rock"));
-console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+// console.log(getRoundResults("Rock"));
+// console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
+}
+
+// showResults("Rock");
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+  roundResultsMsg.innerText = "";
+  winnerMsgElement.innerText = "";
+}
+
+resetGameBtn.addEventListener("click", resetGame);
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
